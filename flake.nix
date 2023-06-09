a5774742d40c6bf309314dd03774a56a29fb005c
Sergey Frolov <sergeyfrolov1995@yahoo.com>
2023-06-10 01:08:30 +0300

    more architectures

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   outputs = { self, nixpkgs }: 
   let
-  supportedSystems = ["aarch64-linux" "x86_64-linux" ];
+  supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+  
   forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
   nixpkgsFor = forAllSystems ( system: import nixpkgs { inherit system; } );
