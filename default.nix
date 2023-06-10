9e4bb2572e918e57cffd6c76ac04066984bebc9e
sergey frolov on nixos <sergeyfrolov1995@yahoo.com>
2023-06-10 16:31:30 +0300

    Added runtime dependency on xclip package

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 {
 	stdenv,
-  fetchgit,
+        fetchgit,
+        xclip,
+	makeWrapper,
+	lib,
 	...
 }: 
 stdenv.mkDerivation {
@@ -12,21 +15,27 @@ stdenv.mkDerivation {
     url = "https://github.com/PORNOZVEZDA/cpwd_for_nix";
     rev = "14ce3ef9cfbd977e1e3dbc348bbff9aca1dd4e44";
     sha256 = "1h7nhg7ym5rnxlahxklp4ccdyybvjhl2a594k1dic620w0dnk2zd";
-
   };
-	builtInputs = [];
+  nativeBuildInputs = [xclip makeWrapper];
+
+  ## this variable is not needed to use xclip in runtime..
+  #buildInputs = [xclip makeWrapper];
   buildPhase = ''
+  export PATH=$PATH:/$xclip/bin
   cd $src
   mkdir -p $out/bin
   cpwd_out=$out/bin/cpwd
   $src/build.sh $cpwd_out
-  #ls $src
 
   '';
 
   installPhase = ''
   # there could be step with copying compiled binary to $out/bin/out name
   # but we already do it during build step.
-
   '';
+
+# specify runtime dependency on xclip for cpwd. Won't find it otherwise
+  postFixup = ''
+  wrapProgram $out/bin/cpwd --prefix PATH : ${lib.makeBinPath [xclip]}
+'';
 }
